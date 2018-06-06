a1d9128ee5a04f9ccbde23f8d21ccd3b6cdf9de7
Toni Mas <antoni.mas@gmail.com>
2018-06-06 15:37:13 +0200

    Initial commit

---
+++ app/main/routes.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from flask import render_template, redirect, url_for, request, current_app
+from flask_login import current_user, login_required
+from app import db
+from app.models import User
+from app.main import bp
+
+
+@bp.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
+@login_required
+def index():
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main.index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', next_url=next_url,
+                           prev_url=prev_url)
---
+++ app/models.py
@@ -0,0 +1,411 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 Toni Mas <toni.mas@bluekiri.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+from app import db, login
+from sqlalchemy import DateTime, Sequence, text
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import Config as config
+
+##########################################################################
+#
+# The schema version is used to track when upgrades are needed to the
+# configuration database. Increment this whenever changes are made to the
+# model or data, AND ensure the upgrade code is added to setup.py
+#
+##########################################################################
+
+SCHEMA_VERSION = 0
+
+db.metadata.schema = config.DATABASE_SCHEMA
+
+
+class Business(db.Model):
+    """
+    Business table
+    """
+    __tablename__ = 'business'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('bussiness_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    name = db.Column(
+        db.String(length=255),
+        nullable=False,
+        unique=True
+    )
+    domain = db.Column(
+        db.String(),
+        nullable=False
+    )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+
+
+class Cluster(db.Model):
+    """
+    Cluster table
+    """
+    __tablename__ = 'cluster'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('cluster_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    business_id = db.Column(
+        db.Integer,
+        db.ForeignKey('business.id'),
+        nullable=False
+    )
+    name = db.Column(
+        db.String(length=255),
+        nullable=False,
+        unique=True
+    )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+    domainprefix = db.Column(
+        db.String(),
+        default='pgc',
+        server_default="pgc",
+        nullable=False
+    )
+
+
+class Rule(db.Model):
+    """
+    Rule table
+    """
+    __tablename__ = 'rule'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('backup_config_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    name = db.Column(
+        db.String(length=128),
+        nullable=False
+    )
+    value = db.Column(
+        db.String(length=128),
+        nullable=True
+    )
+    business_id = db.Column(
+        db.Integer,
+        db.ForeignKey('business.id'),
+        nullable=False
+    )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+
+
+class Node(db.Model):
+    """
+    Node table
+    """
+    __tablename__ = 'node'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('nodes_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    cluster_id = db.Column(
+        db.Integer,
+        db.ForeignKey('cluster.id'),
+        nullable=False
+    )
+    name = db.Column(
+        db.String(length=255),
+        nullable=False,
+        unique=True
+    )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+
+
+class Database(db.Model):
+    """
+    Database table
+    """
+    __tablename__ = 'database'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('database_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    name = db.Column(
+        db.String(length=255),
+        nullable=False,
+        unique=True
+    )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+
+
+class Environment(db.Model):
+    """
+    Environment table
+    """
+    __tablename__ = 'environment'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('environment_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    name = db.Column(
+        db.String(length=32),
+        nullable=False,
+        unique=True
+    )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+    domainprefix = db.Column(
+        db.String(length=12),
+        nullable=True
+    )
+
+
+class Deployment(db.Model):
+    """
+    Deployment table
+    """
+    __tablename__ = 'deployment'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('deployment_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    environment_id = db.Column(
+        db.Integer,
+        db.ForeignKey('environment.id'),
+        nullable=False
+    )
+    cluster_id = db.Column(
+        db.Integer,
+        db.ForeignKey('cluster.id'),
+        nullable=False
+    )
+    database_id = db.Column(
+        db.Integer,
+        db.ForeignKey('database.id'),
+        nullable=False
+    )
+    name = db.Column(
+        db.String(length=255),
+        nullable=False,
+        unique=True
+    )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+
+
+class BackupHistory(db.Model):
+    """
+    Backup history table
+    """
+    __tablename__ = 'backup_history'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('backup_history_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    timecreated = db.Column(
+        DateTime,
+        nullable=False,
+        default=datetime.datetime.utcnow,
+        server_default=text('NOW()')
+    )
+    cluster_id = db.Column(
+        db.Integer,
+        db.ForeignKey('cluster.id'),
+        nullable=False
+    )
+    database_id = db.Column(
+        db.Integer,
+        db.ForeignKey('database.id'),
+        nullable=False
+    )
+    scheduled = db.Column(
+        db.String(length=12),
+        nullable=False
+    )
+    state = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('false'),
+        nullable=False
+    )
+    size = db.Column(
+        db.Integer,
+        nullable=False,
+        default=0,
+        server_default=text('0')
+    )
+    duration = db.Column(
+        db.Integer,
+        nullable=False,
+        default=0,
+        server_default=text('0')
+    )
+    info = db.Column(
+        db.Text(),
+        nullable=True
+    )
+
+
+class RecoveryHistory(db.Model):
+    """
+    Recovery history table
+    """
+    __tablename__ = 'recovery_history'
+
+    # Fields.
+    id = db.Column(
+        db.Integer,
+        Sequence('recovery_history_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    timecreated = db.Column(
+        DateTime,
+        nullable=False,
+        default=datetime.datetime.utcnow,
+        server_default=text('NOW()')
+    )
+    cluster_id = db.Column(
+        db.Integer,
+        db.ForeignKey('cluster.id'),
+        nullable=False
+    )
+    database_id = db.Column(
+        db.Integer,
+        db.ForeignKey('database.id'),
+        nullable=False
+    )
+    scheduled = db.Column(
+        db.String(length=12),
+        nullable=False
+    )
+    state = db.Column(
+        db.Boolean(),
+        default=True,
+        server_default=text('true'),
+        nullable=False
+    )
+    info = db.Column(
+        db.Text(),
+        nullable=True
+    )
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(
+        db.Integer,
+        Sequence('user_id_seq', start=1, increment=1),
+        primary_key=True,
+        nullable=False
+    )
+    username = db.Column(
+        db.String(64),
+        index=True,
+        unique=True
+    )
+    email = db.Column(
+        db.String(120),
+        index=True,
+        unique=True
+    )
+    password_hash = db.Column(
+        db.String(128)
+    )
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    #@staticmethod
+    #def try_login(username, password):
+        #conn = get_ldap_connection()
+        #conn.simple_bind_s(
+        #    'cn=%s,ou=Users,dc=testathon,dc=net' % username,
+        #    password
+        #)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
