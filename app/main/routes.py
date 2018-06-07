6e4f703e0af54e29f2ac6062bb32d4ef9b4154fe
Toni Mas <antoni.mas@gmail.com>
2018-06-07 15:55:34 +0200

    Adding bootstrap

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import render_template, redirect, url_for, request, current_app
 from flask_login import current_user, login_required
 from app import db
-from app.models import User
+from app.models import User, Business
 from app.main import bp
 
 
@@ -18,11 +18,8 @@ def before_request():
 @login_required
 def index():
     page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().paginate(
-        page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.index', page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('main.index', page=posts.prev_num) \
-        if posts.has_prev else None
+    business = Business.query.all()
+    next_url = None
+    prev_url = None
     return render_template('index.html', title='Home', next_url=next_url,
-                           prev_url=prev_url)
+                           business=business, prev_url=prev_url)
